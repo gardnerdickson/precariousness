f3a416d359e25225855cb13ba1af467fdfea1424
Gardner Dickson <gardner.dickson@gmail.com>
2022-12-21 14:30:11 -0500

    Abstract socket message routing into a common class

--- static/common.js
+++ static/common.js
@@ -4,3 +4,29 @@ function hideAllGameComponents() {
         el.style.display = "none"
     })
 }
+
+
+class SocketMessageRouter {
+
+    constructor() {
+        self.handlers = new Map()
+    }
+
+    addRoute(route, handler) {
+        if (self.handlers.has(route)) {
+            throw Error("Route " + route + " already registered")
+        }
+        self.handlers.set(route, handler)
+    }
+
+    onmessage(event) {
+        console.debug("Received message:", event)
+        const message = JSON.parse(event.data)
+        let operation = message.operation
+        if (self.handlers.has(operation)) {
+            self.handlers.get(operation)(message.payload)
+        } else {
+            console.warn("Encountered unregistered route:", event)
+        }
+    }
+}
